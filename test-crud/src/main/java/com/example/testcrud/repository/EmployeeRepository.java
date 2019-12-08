7a42709e3c09dc5dd3dd897640e0006df376a511
Paulo <paulo86roberto@gmail.com>
2019-12-08 12:10:19 -0400

    project spring boot angular CRUD

---
+++ test-crud/src/main/java/com/example/testcrud/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.testcrud.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.testcrud.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
